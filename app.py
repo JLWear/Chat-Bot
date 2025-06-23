5d0703d162da969bdeaca42702472308e8a7c5e2
JLWear <thomas.noel612@gmail.com>
2025-06-23 14:32:16 +0200

    fix requirement.txt

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from chatbot import OrientationChatbot
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    import subprocess
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
 
 if __name__ == "__main__":
     bot = OrientationChatbot()
